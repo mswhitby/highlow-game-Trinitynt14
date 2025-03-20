31896fb32e3e62242632c1046f31e5404a0cf549
Trinitynt14 <trinnt607@gmail.com>
2025-03-20 16:29:04 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+import random
+random_num = random.randint(1,100)
 
+
+def highlow():
+    guess = int(input("pick a number between 1 and 100: "))
+    while guess != (random_num):
+        guess = int(input("comfirm "))
+        if guess > (random_num):
+            Again = int(input("try a lower number! "))
+        else:
+            if guess < (random_num):
+                now = int(input("try a higher number! "))
+        if guess == (random_num):
+            print("You got it right!")
+
+highlow()
